30a3ec318098ca4ff179319e2788ae4f980dcee2
shamrat077 <shamrat077@gmail.com>
2022-04-11 00:00:53 +0600

    divided biodata app into several component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BioData from "./components/BioData";
+import AcademicInfo from "./components/AcademicInfo";
+import Interest from "./components/Interest";
+import Skills from "./components/Skills";
+import SocialInfo from "./components/SocialInfo";
+import PersonalInfo from "./components/PersonalInfo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BioData name="shamrat" />
+      <hr />
+      {}
+      <PersonalInfo
+        name="shamrat"
+        number="+8801781821292"
+        email="shamrat077@gmail.com"
+      />
+      <hr />
+      <SocialInfo github="github/shamrat077" linkedIn="linkedin/shamrat077" />
+      <hr />
+      <AcademicInfo ssc="2011" hsc="2014" bsc="2019" />
+      <hr />
+      <Skills skills={["html", "css", "javaScript"]} />
+      <hr />
+      <Interest interest={["reading", "coding", "traveling"]} />
     </div>
   );
 }
